75a99b8f17903dc1dbe4c90ce4132d71e4e01693
tuyetlannguyenthi <152297570+tuyetlannguyenthi@users.noreply.github.com>
2024-05-16 20:18:53 +0700

    Delete NguyenThiTuyetLan_Buoi01 directory

--- NguyenThiTuyetLan_Buoi01/buoi1_1.js
+++
@@ -1,6 +0,0 @@
-let masv1=2122110559
-let hoten1="Nguyễn Thị  Tuyết Lan"
-console.log(masv1);
-console.log(hoten1);
-
-document.getElementById("show").innerHTML=`Mã sinh viên: "${masv1}"Họ tên sinh viên: ${hoten1}`;
